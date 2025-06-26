949e382a3b2e30c75ed7e306c56723074eff9aed
Nitish <snkumar6926@gmail.com>
2025-06-26 11:55:44 +0530

    New Changes Added

--- script.js
+++ script.js
@@ -20,3 +20,30 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+// Scrollable Functionality...
+const links = document.querySelectorAll('.sidebar-nav a');
+const section = document.querySelectorAll('main section');
+
+function showSection(id) {
+    sections.forEach(section => {
+        section.classList.remove('active-section');
+    });
+    const target = document.querySelector(id);
+    if (target) target.classList.add('active-section');
+}
+
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        links.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+        const targetId = this.getAttribute('href');
+        showSection(targetId);
+    });
+});
+
+// Show home section by default
+window.onload = () => {
+    showSection('#home');
+};
